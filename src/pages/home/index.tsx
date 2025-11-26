e32dda538fc14fcd47430168d78197a48067e568
murilomunari <munarimurilo441@gmail.com>
2025-11-26 13:35:41 -0300

    feat: add react-icons dependency and implement Social component for link sharing

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,34 @@
+import { Social } from "../../components/Social"
+import { FaLinkedin, FaGithub} from "react-icons/fa"
+
 export function Home() {
     return(
-        <div className="w-full h-screen bg-neutral-900">
-            <h1 className="text-white text-3xl">Pagina home</h1>
+        <div className="flex flex-col w-full py-4 items-center justify-center">
+            <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Murilo DEV</h1>
+            <br />
+            <span className="text-gray-50 mt-3 mb-5">Veja meus links 👇</span>
+
+
+            <main className="flex flex-col w-11/12 max-w-xl text-center">
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+                    <a href="">
+                        <p className="text-base md:text-lg">
+                            canal no youtube
+                        </p>
+                    </a>
+                </section>
+
+                <footer className="flex justify-center gap-3 my-4">
+                    <Social url="https://www.linkedin.com/in/murilomunaribissiato/"> 
+                        <FaLinkedin size={35} color="#FFF"/>
+                    </Social>
+
+                    <Social url="https://github.com/murilomunari"> 
+                        <FaGithub size={35} color="#FFF"/>
+                    </Social>
+
+                </footer>
+            </main>
         </div>
     )
 }
