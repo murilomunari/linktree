f796620be1476a108d10a959627991a546db9c96
murilomunari <munarimurilo441@gmail.com>
2025-11-27 17:36:52 -0300

    fix: update gradient class for logo text in Login component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -37,7 +37,7 @@ export function Login() {
             <Link to="/">
                 <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
                     Dev
-                    <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
+                    <span className="bg-linear-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
                         Link
                     </span>
                 </h1>
